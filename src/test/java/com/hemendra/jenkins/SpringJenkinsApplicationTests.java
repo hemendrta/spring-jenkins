da2057419e386fac6b01958ca15e9bb16d5cf9f7
hemendrta <hemendra.singh.c@outlook.com>
2021-03-18 08:46:57 -0700

    first commit

---
+++ src/test/java/com/hemendra/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,23 @@
+package com.hemendra.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	private static Logger logger= LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+
+	@Test
+	void contextLoads() {
+
+		logger.info("Running the test cases!!!");
+
+	}
+
+}
